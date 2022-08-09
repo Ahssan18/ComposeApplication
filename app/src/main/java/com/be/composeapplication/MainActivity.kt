9bce729e5c30abe0901c9a04b312bd5ec7bcf8e3
Ahssan18 <asifilyani@gmail.com>
2022-08-09 23:58:17 +0500

    list in compose

--- app/src/main/java/com/be/composeapplication/MainActivity.kt
+++ app/src/main/java/com/be/composeapplication/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.be.composeapplication
 
-import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.be.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,38 +26,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Message(val id: Int, val title: String, val description: String)
+
 @Composable
 fun MyCompose(msg: String) {
-    showPreview(msg)
+    var list = ArrayList<Message>()
+    list.add(Message(1, "ahssan", "goodboy"))
+    list.add(Message(2, "ahssan", "goodboy"))
+    list.add(Message(1, "ahssan", "goodboy"))
+    list.add(Message(2, "ahssan", "goodboy"))
+    list.add(Message(1, "ahssan", "goodboy"))
+
+    showPreview(list)
 }
 
 @Composable
-fun showPreview(msg: String) {
-    val context = LocalContext.current
-    var i: Int = 0
-    Row(Modifier.background(Color.Red)) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "this is first image insert in compose",
-            Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, Color.Blue, CircleShape)
-                .clickable {
-                    Log.e("Image", "Hurra" + i++)
-                    Toast
-                        .makeText(
-                            context, "Hurra", Toast.LENGTH_LONG
-                        )
-                        .show()
-                }
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        Column() {
-            Text(text = "$msg", color = MaterialTheme.colors.onPrimary)
-            Text(text = "develop by ahssan")
+fun showPreview(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            showCaht(message)
         }
+
+
     }
 
+
+}
+
+@Composable
+fun showCaht(message: Message) {
+    val context = LocalContext.current
+    var align:TextAlign
+    if (message.id == 1) {
+        align = TextAlign.Right
+    } else {
+        align = TextAlign.Left
+
+    }
+    Text(text = message.title,modifier = Modifier.fillMaxWidth().padding(10.dp), textAlign = align)
 
 }
